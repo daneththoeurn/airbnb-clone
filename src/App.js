b0be8edc17f3cbca1810b4a748a5d0c66781b38d
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-21 00:05:49 +0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
-
+import Home from './Home';
+import Header from './Header';
 function App() {
   return (
-
+  
     //BEM
     <div className="app">
-     <h1>Let's build AIRBNB clone!</h1>
+       <Header/>
+      <Home/>
+     
+    
+     {/* Home */}
+        {/* Header */}
+
+        {/* Banner */}
+           {/* Search */}
+
+        {/* Cards */}
+
+        {/* Footer */} 
+
+     {/* SearchPage */}   
+       {/* ... */}
     </div>
   );
 }
