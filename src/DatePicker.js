026ff2ae39d315070e2fbe623f904665b48385f2
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-30 00:42:12 +0700

    finshed

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -5,8 +5,10 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 import PeopleIcon from "@material-ui/icons/People";
 import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 function DatePicker() {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -28,7 +30,7 @@ function DatePicker() {
       </h2>
       <input min={0} defaultValue={2} type="number"></input>
       
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => history.push('/search')}>Search Airbnb</Button>
     </div>
   );
 }
